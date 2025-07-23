628e101f71b0e96953e300969c2b72b7cc37d6f4
terence <2301615451@qq.com>
2025-07-23 11:01:00 -0700

    TL - now using last 5 digits phone# to create user

--- utils/validation.js
+++ utils/validation.js
@@ -1,36 +1,31 @@
 // Phone number validation function
 const validatePhoneNumber = (phone) => {
-    // Remove any non-digit characters
-    const cleanPhone = phone.replace(/\D/g, '');
-  
-    if (cleanPhone.length === 0) {
-      return {
-        isValid: false,
-        error: 'Please enter a phone number'
-      };
-    }
-  
-    if (cleanPhone.length !== 10) {
-      return {
-        isValid: false,
-        error: 'Phone number must be exactly 10 digits'
-      };
-    }
-  
-    if (/^[01]/.test(cleanPhone)) {
-      return {
-        isValid: false,
-        error: 'Phone number cannot start with 0 or 1'
-      };
-    }
-  
+  // Remove any non-digit characters
+  const cleanPhone = phone.replace(/\D/g, '');
+
+  if (cleanPhone.length === 0) {
     return {
-      isValid: true,
-      cleaned: cleanPhone,
-      formatted: `(${cleanPhone.slice(0,3)}) ${cleanPhone.slice(3,6)}-${cleanPhone.slice(6)}`
+      isValid: false,
+      error: 'Please enter a phone number'
     };
+  }
+
+  const lastFive = cleanPhone.slice(-5);
+
+  if (lastFive.length !== 5) {
+    return {
+      isValid: false,
+      error: 'Last 5 digits of phone number are required'
+    };
+  }
+
+  return {
+    isValid: true,
+    cleaned: lastFive,
+    formatted: lastFive
   };
-  
-  module.exports = {
-    validatePhoneNumber
-  };
+};
+
+module.exports = {
+  validatePhoneNumber
+};
